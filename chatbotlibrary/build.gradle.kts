df392aa9ca89a51b1545acb9917c991afcdb043d
kumar12345 <nitin.kumar@appventurez.com>
2026-05-24 12:10:51 +0530

    create chat support and chatbot sdk using openAi

--- chatbotlibrary/build.gradle.kts
+++ chatbotlibrary/build.gradle.kts
@@ -47,7 +47,7 @@ afterEvaluate {
                from(components["release"])
                 groupId = "com.github.nitin09542"
                 artifactId = "chatbotlibrary"
-                version = "1.0.0"
+                version = "2.0.0"
             }
         }
     }
